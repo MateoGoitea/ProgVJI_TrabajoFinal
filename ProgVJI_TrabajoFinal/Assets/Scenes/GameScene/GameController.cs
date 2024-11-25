006f6eb6db4c9770f87479da39dbdc9f3c5252f6
MateoGoitea <goiteamateonicolas73@gmail.com>
2024-11-25 15:39:02 -0300

    Correccion_VictoryCondition
    Se corrigio la condicion para que aparezca la pantalla de victoria

--- ProgVJI_TrabajoFinal/Assets/Scenes/GameScene/GameController.cs
+++ ProgVJI_TrabajoFinal/Assets/Scenes/GameScene/GameController.cs
@@ -6,18 +6,20 @@ using UnityEngine.SceneManagement;
 public class GameController : MonoBehaviour
 {
     private GameObject _basePlayer;
-    private GameObject[] _baseEnemy;
+    private GameObject _baseEnemy;
 
     // Start is called before the first frame update
     void Start()
     {
         _basePlayer = GameObject.FindGameObjectWithTag("BasePlayer");
-        _baseEnemy = GameObject.FindGameObjectsWithTag("BaseEnemy");
+        
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   //constantemente busca por un enemy base, cuando ya no quedan ninguno la funcion devuelve true 
+        _baseEnemy = GameObject.FindGameObjectWithTag("BaseEnemy");
+        
         if (_basePlayer == null) SceneManager.LoadScene("GameOver");
 
         if (BasesEnemysDestroy()) SceneManager.LoadScene("Victory");
@@ -25,10 +27,15 @@ public class GameController : MonoBehaviour
 
     private bool BasesEnemysDestroy()
     {
-        foreach (var enemy in _baseEnemy)
+        if (_baseEnemy != null){
+            return false; //si aun queda alguna base
+        }else{
+            return true;
+        }
+        /*foreach (var enemy in _baseEnemy)
         {
             if (_baseEnemy != null) return false;//si aun queda alguna base
         }
-        return true;//si todas fueros destruidas
+        return true;//si todas fueros destruidas*/
     }
 }
